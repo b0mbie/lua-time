5232ec94b6dad70e9d34822827acd755da986574
akabomb-prog <69766525+b0mbie@users.noreply.github.com>
2023-03-03 23:42:39 +0200

    Update usleep.c

--- src/ltime/usleep.c
+++ src/ltime/usleep.c
@@ -12,11 +12,12 @@
 
 void ltime_usleep(lua_Integer usec)
 {
+    // Clamp to minimum 0.
+    if (usec < 0) usec = 0;
 #ifdef LUA_LTIMER_WINDOWS
     HANDLE timer;
     LARGE_INTEGER ft;
-    // Convert to 100 nanosecond interval, negative value
-    // indicates relative time.
+    // Convert to 100 nanosecond interval.
     ft.QuadPart = -(10 * usec);
     timer = CreateWaitableTimer(NULL, TRUE, NULL);
     SetWaitableTimer(timer, &ft, 0, NULL, NULL, 0);
